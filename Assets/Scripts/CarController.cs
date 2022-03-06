d6b568df44ba276f69bfba236ca0aebbd6aaa70d
Zaksley <enzo8550@gmail.com>
2022-03-06 17:04:40 +0100

    adding rotation

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,25 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CarController : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 1500f; 
+    public float rotationSpeed = 15f; 
     private float movement = 0f; 
+    private float rotation = 0f; 
     [SerializeField] private WheelJoint2D backWheel; 
     [SerializeField] private WheelJoint2D frontWheel; 
     
-    
+    private Rigidbody2D rb; 
 
     void Start()
-    {
-        
+    {   
+        rb = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         movement = -Input.GetAxisRaw("Vertical") * speed; 
+        rotation = Input.GetAxisRaw("Horizontal"); 
     }
 
     void FixedUpdate() {
@@ -37,7 +41,9 @@ public class CarController : MonoBehaviour
 
             JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = backWheel.motor.maxMotorTorque }; 
             backWheel.motor = motor; 
-             frontWheel.motor = motor; 
+            frontWheel.motor = motor; 
         }
+
+        rb.AddTorque(-rotation * rotationSpeed * Time.fixedDeltaTime); 
     }
 }
