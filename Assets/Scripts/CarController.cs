26a1538ab2392ca325b60989017224c60b532ca7
Zaksley <enzo8550@gmail.com>
2022-03-18 23:08:24 +0100

    Reworking the whole car system

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,76 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class CarController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speedMax = 15000f; 
-    public float speedAcceleration = 300f; 
-    public float speedBrake = 800f;
-    public float rotationSpeed = 15f; 
-    private float movement = 0f; 
+
+    [SerializeField] private float speed = 20f;
+    private bool move; 
+    private bool isGrounded; 
+
+
     private float rotation = 0f; 
-    [SerializeField] private WheelJoint2D backWheel; 
-    [SerializeField] private WheelJoint2D frontWheel; 
-    
+    [SerializeField] private float rotationSpeed = 2f; 
+
     private Rigidbody2D rb; 
 
     void Start()
     {   
         rb = GetComponent<Rigidbody2D>(); 
-        speedMax = -speedMax;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Get inputs 
+        rotation = -Input.GetAxisRaw("Horizontal"); 
         
-        rotation = Input.GetAxisRaw("Horizontal"); 
-
-        // if previously 0, start motor with speed
-
-        /*
-        if (movement == 0f) 
-        {
-            movement = -Input.GetAxisRaw("Vertical") * speed;
-        }
-        else 
-        {
-            movement += -Input.GetAxisRaw("Vertical") * incrSpeed * Time.deltaTime; 
-        }
-        */
-
-        // Acceleration and brake
-        if (-Input.GetAxisRaw("Vertical") == -1f)       movement += -Input.GetAxisRaw("Vertical") * speedAcceleration * Time.deltaTime;
-        else if (-Input.GetAxisRaw("Vertical") == 1f)   movement += -Input.GetAxisRaw("Vertical") * speedBrake * Time.deltaTime;
-        
-        // Controls motor
-        if (movement > 0)   movement = 0;
-        if (movement <= speedMax) movement = speedMax; 
+        if (Input.GetAxisRaw("Vertical") > 0)       move = true; 
+        else                                        move = false; 
+                
     }
 
-    void FixedUpdate() {
+    void FixedUpdate() 
+    {
 
-        if (movement == 0f) 
+        if (move && isGrounded)
         {
-            backWheel.useMotor = false; 
-            frontWheel.useMotor = false; 
-            JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = backWheel.motor.maxMotorTorque }; 
-            backWheel.motor = motor; 
-            frontWheel.motor = motor; 
-        }
-        else 
-        {
-            backWheel.useMotor = true; 
-            frontWheel.useMotor = true; 
-
-            JointMotor2D motor = new JointMotor2D { motorSpeed = movement, maxMotorTorque = backWheel.motor.maxMotorTorque }; 
-            backWheel.motor = motor; 
-            frontWheel.motor = motor; 
+            rb.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force); 
         }
 
-        rb.AddTorque(-rotation * rotationSpeed * Time.fixedDeltaTime); 
+        if (rotation != 0) 
+        {
+            rb.AddTorque(rotation * rotationSpeed * rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+        }
+
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isGrounded = true; 
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false; 
+    }
+
 }
